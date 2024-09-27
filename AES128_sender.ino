fac0c7985e8e90971d4a3f1b260011848e362f87
john041 <70787077+john041@users.noreply.github.com>
2024-09-26 20:18:48 -0600

    Update AES128_sender.ino

--- AES128_sender.ino
+++ AES128_sender.ino
@@ -10,12 +10,14 @@ WiFiClient wifiClient;                        //Object to set up Wifi
 PubSubClient MQTTClient(wifiClient);          //Object to set up MQTT client
 AES128 aes;                                   //Object of AES encryption
 
-unsigned long roundTripTime = 0;              //Variables to keep track of time and number of packets sent
+unsigned long roundTripTime = 0;              //Variables to keep track of time, number of packets sent, and free memory
 unsigned long startRoundTripTime = 0;
 unsigned long encryptTime = 0;
 unsigned long startEncryptTime = 0;
 unsigned long decryptTime = 0;
 unsigned long startDecryptTime = 0;
+unsigned long freeMemoryEncrypt = 0;
+unsigned long freeMemoryDecrypt = 0;
 int number = 0;
 
 char key[] = "Thisisatestaaaa!";               //Key used in encryption process
@@ -34,28 +36,40 @@ void convertFromString( const char* info, byte* memory ) {
   }
 }
 
+//Prints a byte array to serial output
+//  parameter 1 byte to print
+//  parameter 2 int of the size of the byte array
 void printByte( byte* info, int sizeOfArray) {
   for( int i = 0; i < sizeOfArray; i++ ) {
     Serial.write(info[i]);
   }
 }
 
+//Encrypts a char array while messuring time and free memory
+//  parameter 1 const char array to encrypt
 void runEncryption(const unsigned char* payload) {
    startEncryptTime = micros();
-   aes.encryptBlock(encryptedData, payload);
-   encryptTime = micros() - startEncryptTime;
+   aes.encryptBlock(encryptedData, payload);                     //Encrypt the char array
+   encryptTime = micros() - startEncryptTime;                    //Messure the time to encrypt
+   freeMemoryEncrypt = ESP.getFreeHeap();                        //Messure the free memory space
 }
 
+//Decrypts a char array while messuring time and free memory
+//  parameter 1 const char array to decrypt
 void runDecryption(const unsigned char* payload) {
    startDecryptTime = micros();
-   aes.decryptBlock(decryptedData, payload);
-   decryptTime = micros() - startDecryptTime;
+   aes.decryptBlock(decryptedData, payload);                     //Encrypt the char array
+   decryptTime = micros() - startDecryptTime;                    //Messure the time to encrypt
+   freeMemoryDecrypt = ESP.getFreeHeap();                        //Messure the free memory space
 }
 
+//MQTT functions that runs when MQTT message is recieved
+//  parameter 1 char pointer that stores the topic that the message comes from
+//  parameter 2 byte pointer that contains the message of MQTT messsage
+//  parameter 3 unsigned int of the length of the message
 void recieve(char* topic, byte* message, unsigned int length) {
-  //Serial.println(topic);
   roundTripTime = micros() - startRoundTripTime;
-  Serial.print("Packet Sent:#");
+  Serial.print("Packet Sent:#");                                //Print roundtrip time, encrypt time, and decrypt time
   Serial.print(number);
   Serial.print("#Round Trip Time#");
   Serial.print(roundTripTime);
@@ -64,47 +78,52 @@ void recieve(char* topic, byte* message, unsigned int length) {
   Serial.print("#Encryption Time Sender#");
   Serial.print(decryptTime);
   Serial.print("#Message:#");
-  runDecryption(message);
+  runDecryption(message);                                       //Decrypt MQTT message
   printByte(message, length);
-  printByte(decryptedData, sizeof(decryptedData));
-  Serial.println();
+  Serial.print("#");
+  printByte(decryptedData, sizeof(decryptedData));              //Print decrypted message and memory messurments
+  Serial.print("#");
+  Serial.print(freeMemoryEncrypt);
+  Serial.print("#");
+  Serial.print(freeMemoryDecrypt);
+  Serial.print("#");
+  Serial.println(ESP.getFreeHeap());
 }
 
+//Setup code here, to run once:
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);                                                 //Set clock for serial
+  Serial.begin(9600);                                            //Set clock for serial
 
   Serial.println("Connecting to WiFi");                                 
-  WiFi.begin(nameWifi, password);                                     //Connect to Wifi Network
+  WiFi.begin(nameWifi, password);                                //Connect to Wifi Network
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("-");
   }
-  
-  Serial.println("Wifi connected");                                   //Send message that Wifi connection   
+  Serial.println("Wifi connected");                              //Send message that Wifi connection   
 
-  MQTTClient.setServer("192.168.1.113", 1883);
+  MQTTClient.setServer("192.168.1.113", 1883);                   //MQTT settings setup               
   MQTTClient.setCallback(recieve);
   
-  if(MQTTClient.connect("Sensor")) {
+  if(MQTTClient.connect("Sensor")) {                             //Connect to MQTT broker
     Serial.println("Connected to Broker");    
   } else {
     Serial.println("Problem connecting to Broker");
   }
 
-  MQTTClient.subscribe("/test/reciever");
-  convertFromString(key, keyMemory);
+  MQTTClient.subscribe("/test/reciever");                       //Subscribe to topic to listen to
+  convertFromString(key, keyMemory);                            //Set up encryption key
   aes.setKey(keyMemory, 16);
 }
 
-//main code here, runs repeatedly
+//Main code here, runs repeatedly:
 void loop() {
-  MQTTClient.loop();
-  delay(4000);
-  if(number < 200) {
+  MQTTClient.loop();                                           //Check for MQTT messages
+  delay(1000);
+  if(number < 200) {                                           //Send 200 packets every 1 second
     number = number + 1;
     convertFromString("SendDistAndTimes", messageMemory);
     startRoundTripTime = micros();
-    runEncryption(messageMemory);
+    runEncryption(messageMemory);                              //Encrypt message and send message
     bool result = MQTTClient.publish("/test/sender", encryptedData, sizeof(encryptedData), false);                 
   }
 }
